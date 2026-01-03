d38e82ba28a3208f7af17a469ab053d488caf321
Shivam191205 <shivamnagpal603@gmail.com>
2026-01-03 17:13:47 +0530

    Add files via upload

---
+++ musicData.js
@@ -0,0 +1,59 @@
+// AUTO-GENERATED FILE (do not edit manually)
+const MUSIC = {
+  "ArjanDhillon": [
+    "25-25 (Official Video) Arjan Dhillon _ Mxrci _ Gold Media _ Latest Punjabi Song_Brown Studios(M4A_128K).m4a",
+    "Arjan Dhillon - Dass Billo (New Song) Awara Album(M4A_128K).m4a",
+    "Arjan Dhillon - Dope (New Song) Awara Album(M4A_128K).m4a",
+    "Jatt di jaaneman.m4a",
+    "Kalli Sohni (Full Video) _ Arjan Dhillon _Proof_Gold Media_Brown Studios_Latest Punjabi Songs 2021(M4A_128K).m4a",
+    "Mandeer.m4a",
+    "PHOTO (Full Video) Nimrat Khaira _ Arjan Dhillon _ Preet Hundal _ Latest Punjabi Song _Brown Studios(M4A_128K).m4a"
+  ],
+  "karanAujla": [
+    "2 AM (Full Video) Karan Aujla _ Roach Killa _ Rupan Bal _ Latest Punjabi Songs 2019(MP3_160K).mp3",
+    "52 Bars  (Official Video) Karan Aujla _ Ikky _ Four You EP _ First Song _ Latest Punjabi Songs 2023(M4A_128K).m4a",
+    "80 Degrees(M4A_128K).m4a",
+    "Feel The Flava (It_z All Good)(M4A_128K).m4a",
+    "Few Days (Full Video) Karan Aujla I Amantej Hundal _ Yeah Proof _ Latest Punjabi Songs 2021(M4A_128K).m4a",
+    "Ford (Loud AF)(M4A_128K).m4a",
+    "HINT (Full Video) Karan Aujla _ Rupan Bal _ Jay Trak _ Latest Punjabi Songs 2019(MP3_160K).mp3",
+    "Karan Aujla - 7.7 Magnitude (Official Audio)(MP3_160K) (1).mp3",
+    "Karan Aujla - Boyfriend (Official Audio)(MP3_160K).mp3",
+    "Karan Aujla - Daytona (Official Audio)(MP3_160K) (1).mp3",
+    "Karan Aujla - For A Reason (Official Audio)(MP3_160K) (1).mp3",
+    "Karan Aujla - MF Gabhru_ (Official Audio)(MP3_160K) (1).mp3",
+    "Take It Easy(M4A_128K).m4a",
+    "They Know (full song) Karan Aujla x Ikka _ way ahead _ new song leaked(M4A_128K).m4a"
+  ],
+  "Mahadev": [
+    "Bam Bhole Bam Bhole Bam ( GURDAS MANN).mp4(M4A_128K).m4a",
+    "Bolo Har Har.m4a",
+    "Namo Namo.m4a"
+  ],
+  "Remedial": [
+    "2 Raflaan (Full Video) Mankirt Aulakh Ft Gurlez Akhtar _ Shree Brar _ Desi Crew _ New Punjabi Songs(M4A_128K).m4a",
+    "Bhalwani Gedi (Official Song) Jassa Dhillon _ Gur sidhu _ New punjabi Songs 2021 _Punjabi Songs 2021(M4A_128K).m4a",
+    "C Walk(M4A_128K).m4a",
+    "Casanova (Official Video) Jerry _ Devilo _ Hrprt Brar _ Yaarvelly Production_ New Punjabi Songs 2021(M4A_128K).m4a",
+    "Diljit Dosanjh_ Peaches (Official Music Video) Intense _ Raj Ranjodh _ Drive Thru(M4A_128K).m4a",
+    "Her (Official Audio) - Shubh(M4A_128K).m4a",
+    "Majha Block (Full Video) Prem Dhillon _ Roopi Gill _ Sanb _ Sukh Sanghera _ New Punjabi Songs 2020(M4A_128K)_1.m4a",
+    "SCHEDULE (OFFICIAL VIDEO) _ TEGI PANNU _ MANNI SANDHU _ LATEST PUNJABI SONGS 2021(M4A_128K).m4a",
+    "Sift.m4a"
+  ],
+  "Sidhumoosewala": [
+    "295 (Official Audio) _ Sidhu Moose Wala _ The Kidd _ Moosetape(M4A_128K).m4a",
+    "B Town.mp3",
+    "Badfella.m4a",
+    "BUILT DIFFERENT (Official Audio) Sidhu Moose Wala _ The Kidd _ Moosetape(M4A_128K).m4a",
+    "Calaboose (Official Video) Sidhu Moose Wala _ Snappy _ Moosetape(M4A_128K).m4a",
+    "Famous.mp3",
+    "G Shit (Full Video) Sidhu Moose Wala _ Blockboi Twitch _ The Kidd _ Sukh Sanghera _ Moosetape(M4A_128K).m4a",
+    "Jatt da Muqabla.mp3",
+    "My Block.m4a",
+    "OLD_SKOOL_(Full_Video)_Prem_Dhillon_ft_Sidhu_Moose_Wala_Naseeb_New_Song_2020.mp3",
+    "Sidhu Son (Official Audio) Sidhu Moose Wala _ The Kidd _ Moosetape(M4A_128K).m4a",
+    "These Days (Official Audio) _ Sidhu Moose Wala _ Bohemia _ The Kidd _ Moosetape(M4A_128K).m4a",
+    "US (Official Video) Sidhu Moose Wala _ Raja Kumari _ The Kidd _ Sukh Sanghera _ Moosetape(M4A_128K).m4a"
+  ]
+};
